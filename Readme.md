e17a746baee071a856fe8d7f595451dfe08a6ddf
sylarknows-glitch <sylarknows@gmail.com>
2025-11-14 15:25:47 +0800

    add js

--- Readme.md
+++
@@ -1,4 +0,0 @@
-#hello-world
-
-Hello World repository for Git
-This is an example repository for the Git
---
+++ hello.js
@@ -0,0 +1,6 @@
+
+const outputElement = document.getElementById("message");
+
+outputElement.textContent = "Hello, World! (Updated by JavaScript)";
+
+console.log("The webpage has been updated!");
